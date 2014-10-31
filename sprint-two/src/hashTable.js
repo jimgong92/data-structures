c1dd1f42a1c2fd462e8784f1e869b6329d7f2352
jimgong92 <gong.jim@gmail.com>
2014-10-31 12:40:57 -0700

    Refactored HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,30 +1,53 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
-  this._collisions = {};
+  // this._collisions = {};
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var values = this._storage.get(i) || [];
-  if (values.length > 0) {
-    this._collisions[k] = values.length;
+  var bucket = this._storage.get(i) || [];
+  var isFound = false;
+  for (var j = 0; j < bucket.length; j++) {
+    var val = bucket[j];
+    if (val[0] === k) {
+      val[1] = v;
+      isFound = true;
+      break;
+    }
   }
-  values.push(v);
-  this._storage.set(i, values);
+  if (!isFound) {
+    var newItem = [k, v];
+    bucket.push(newItem);
+  }
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucketIndex = this._collisions[k] || 0;
-  return this._storage.get(i)[bucketIndex];
+  var bucket = this._storage.get(i);
+  if (!bucket) {
+    return null;
+  }
+  else {
+    var res = null;
+    _.each(bucket, function(item) {
+      if(item[0] === k) res = item[1];
+    });
+    return res;
+  }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucketIndex = this._collisions[k] || 0;
-  this._storage.get(i)[bucketIndex] = null;
-  delete this._collisions[k];
+  var bucket = this._storage.get(i);
+  _.each(bucket, function(item, index, bucket) {
+    if (item[0] === k) {
+      bucket.splice(index, 1);
+    }
+  });
+  if (bucket.length === 0) this._storage.set(i, null);
+
 };
 
 
@@ -32,3 +55,5 @@ HashTable.prototype.remove = function(k){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//
